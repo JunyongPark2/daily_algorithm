f9d7a8a3453538d0e6c4ec98c473e1acc47035c0
JunyongPark2 <shewco3@gmail.com>
2021-02-10 23:37:57 +0900

    210210

--- baekjoon/10_재귀/03_2447_별 찍기 - 10.py
+++ baekjoon/10_재귀/03_2447_별 찍기 - 10.py
@@ -1,6 +1,22 @@
+def star(n):
+    global output
+
+    if n == 3:
+        output[0][:3] = ['*', '*', '*']
+        output[1][:3] = ['*', ' ', '*']
+        output[2][:3] = ['*', '*', '*']
+    else:
+        nn = n // 3
+        star(nn)
+        for row in range(3):
+            for col in range(3):
+                if row == 1 and col == 1:
+                    continue
+                for k in range(nn):
+                    output[nn*row + k][nn*col:nn*(col+1)] = output[k][:nn]
 N = int(input())
-a = '''***
-* *
-***
-'''
-print(a*2)
+
+output = [[' ' for _ in range(N)] for _ in range(N)]
+star(N)
+for line in output:
+    print(''.join(line))
--- baekjoon/10_재귀/04_11729_하노이 탑 이동 순서.py
+++ baekjoon/10_재귀/04_11729_하노이 탑 이동 순서.py
@@ -1,1 +1,14 @@
+def move_a_to_c(k, a, b, c):
+    if k == 1:
+        move_list.append([a, c])
+    else:
+        move_a_to_c(k-1, a, c, b)
+        move_a_to_c(1, a, b, c)
+        move_a_to_c(k-1, b, a, c)
+
 N = int(input())
+move_list = []
+move_a_to_c(N, '1', '2', '3')
+print(len(move_list))
+for move in move_list:
+    print(' '.join(move))
